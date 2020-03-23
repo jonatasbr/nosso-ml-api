093f7ebbace5fc04e084f6f4475f248fa751d1b9
Jonatas Braz de Sousa <jonatasbraz@gmail.com>
2020-03-23 02:14:59 -0300

    Update users.controller.ts

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,23 +1,40 @@
-import { Controller, Get, Param, Body, Post } from '@nestjs/common';
-import { UsersService } from './users.service';
+import {
+  Controller,
+  Body,
+  Post,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+import { Repository } from 'typeorm';
 import { User } from './user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserDTO } from './user.dto';
 
 @Controller('users')
 export class UsersController {
-  constructor(private userService: UsersService) {}
-
-  @Get()
-  async getIndex(): Promise<User[]> {
-    return await this.userService.findAll();
-  }
-
-  @Get(':id')
-  async getShow(@Param('id') id): Promise<User> {
-    return await this.userService.findOne(id);
-  }
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {}
 
   @Post()
-  create(@Body() user: User) {
-    return this.userService.create(user);
+  async create(@Body() userDTO: UserDTO) {
+    const userExists: User = await this.usersRepository.findOne({
+      where: { login: userDTO.login },
+    });
+
+    if (userExists) {
+      throw new HttpException(
+        {
+          status: HttpStatus.BAD_REQUEST,
+          error: 'Email is already registered',
+        },
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    const user = new User(userDTO.login, userDTO.password);
+
+    return await this.usersRepository.save(user);
   }
 }
