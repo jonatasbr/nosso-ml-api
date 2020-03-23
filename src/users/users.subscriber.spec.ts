f1b7a39b2ec336986b09702d793ebecc89c560a5
Jonatas Braz de Sousa <jonatasbraz@gmail.com>
2020-03-23 02:17:14 -0300

    Delete users.subscriber.spec.ts

--- src/users/users.subscriber.spec.ts
+++
@@ -1,7 +0,0 @@
-import { UsersSubscriber } from './users.subscriber';
-
-describe('UsersSubscriber', () => {
-  it('should be defined', () => {
-    expect(new UsersSubscriber()).toBeDefined();
-  });
-});
